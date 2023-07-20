8ebdafb7d7888d56e8fe33cb62b2aa42000fd4d3
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-07-20 11:31:28 +0530

    Create simple form and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <form>
+        <label>First name</label>
+        <input type="text"/>
+        
+        <label>Last name</label>
+        <input type="text"/>
+        
+        <label>Email</label>
+        <input type="email"/>
+        
+        <label>Age</label>
+        <input type="number"/>
+        
+        <label>Password</label>
+        <input type="text"/>
+        
+        <label>Confirm password</label>
+        <input type="text"/>
+
+        <button type="submit">Submit</button>
+      </form>
     </main>
   )
 }
